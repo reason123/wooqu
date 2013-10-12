8b9473e7b5ee6b68b57da14cfb9401cdd8ae7392
cathayandy <wzm_andy@126.com>
2013-10-13 02:38:51 +0800

    news

--- application/views/homepage/news.php
+++ application/views/homepage/news.php
@@ -1,13 +1,16 @@
-<link href = "/page/css/newhome.css" rel = "stylesheet">
+﻿<link href = "/page/css/newhome.css" rel = "stylesheet">
 <script src = "/page/js/newhome.js"></script>
 <div class="newscontainer">
-  <?php $a = 0;$color=array('red','green','blue');?>
+  <?php $color=array('green','blue');?>
   <?php foreach($news_list as $key => $newsInfo): ?>
-  <div class="newswrapper <?php echo $color[$a%3];$a += 1;?>" data-url="<?php echo $newsInfo['url']?>">
+  <div class="newswrapper <?php echo $color[$newsInfo['type']];?>" data-url="<?php echo $newsInfo['url']?>">
     <img class="newsimg" src="<?php echo $newsInfo['imgurl']?>"></img>
     <div class="newstext">
-      <p class="newstitle"><?php echo $newsInfo['title']?></p>
+      <p class="newstotaltext">已报名<span class="newstotalnum"></span>人</p>
+	  <p class="newstitle"><?php echo $newsInfo['title']?></p>
       <p class="newscontent"><?php echo $newsInfo['shortdescription']?></p>
+	  <p class="newsstarttime"></p>
+	  <p class="newsendtime"></p>
     </div>
   </div>
   <?php endforeach ?>
--- page/js/newhome.js
+++ page/js/newhome.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$(".newswrapper").hover(function() {
-		$(this).fadeTo(200, 0.9);
+		$(this).fadeTo(200, 0.6);
 	}, function() {
 		$(this).fadeTo(200, 1.0);
 	});
 	
-	$(".newswrapper").click(function(){
+	$(".newswrapper").click(function() {
 		window.location.href=$(this).data('url');
 	})
 });
