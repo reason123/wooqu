de21cea422f2a4d1e5f381790e9dd4fcfabe4bbd
dreamszl <chenao3220@gmail.com>
2014-03-25 00:07:34 +0800

    Fix act-info description bug

--- application/views/homepage/news_v2.php
+++ application/views/homepage/news_v2.php
@@ -85,13 +85,14 @@
 <div class="act-container">
   <h4>活动列表</h4>
   <div class="act-list">
+     <?php foreach($news_list as $key => $newsInfo): ?>
     <div class="act-info">
       <div class="act-pic">
-        <img src="/page/img/right_1.png">
+        <img src="<?php echo $newsInfo['imgurl']?>">
       </div>
       <div class="act-content">
-        <div class="title">清华水果图团购（第四周）</div>
-        <div class="description">需要自提的同学请提前一天下单</div>
+        <div class="title"><?php echo $newsInfo['title']?></div>
+        <div class="description"><?php echo $newsInfo['shortdescription']?></div>
         <div class="time">
           <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
           <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
@@ -107,64 +108,8 @@
         <div class="user">
           发起者：<span class="value">hellothu</span>
         </div>
-        <div class="time">
-          剩余时间：<span class="value">10小时10分</span>
-        </div>
       </div>
     </div>
-    <div class="act-info">
-      <div class="act-pic">
-        <img src="/page/img/right_1.png">
-      </div>
-      <div class="act-content">
-        <div class="title">清华水果图团购（第四周）</div>
-        <div class="description">需要自提的同学请提前一天下单</div>
-        <div class="time">
-          <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
-          <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
-        </div>
-      </div>
-      <div class="act-join">
-        <div class="num">
-          <span class="value">217</span>人已参加
-        </div>
-        <a href="#" class="join">立即参加</a>
-      </div>
-      <div class="act-user">
-        <div class="user">
-          发起者：<span class="value">hellothu</span>
-        </div>
-        <div class="time">
-          剩余时间：<span class="value">10小时10分</span>
-        </div>
-      </div>
-    </div>
-    <div class="act-info">
-      <div class="act-pic">
-        <img src="/page/img/right_1.png">
-      </div>
-      <div class="act-content">
-        <div class="title">清华水果图团购（第四周）</div>
-        <div class="description">需要自提的同学请提前一天下单</div>
-        <div class="time">
-          <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
-          <div class="start-date">订购开始时间：<span class="value">2014-03-22 21:00:00</span></div>
-        </div>
-      </div>
-      <div class="act-join">
-        <div class="num">
-          <span class="value">217</span>人已参加
-        </div>
-        <a href="#" class="join">立即参加</a>
-      </div>
-      <div class="act-user">
-        <div class="user">
-          发起者：<span class="value">hellothu</span>
-        </div>
-        <div class="time">
-          剩余时间：<span class="value">10小时10分</span>
-        </div>
-      </div>
-    </div>
+  <?php endforeach ?>
   </div>
 </div>
