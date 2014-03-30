607c4b9296009b7cb3f25b41d0a46e9422abaa47
dreamszl <chenao3220@gmail.com>
2014-03-31 03:45:48 +0800

    Add compensate page

--- application/controllers/staticpages.php
+++ application/controllers/staticpages.php
@@ -12,6 +12,12 @@ class Staticpages extends CI_Controller{
         $this->load->view('static/howtouse');
         $this->load->view('base/footer');
     }
+
+    function compensate(){
+        $this->load->view('base/header',array('page'=>'howtouse'));
+        $this->load->view('static/compensate');
+        $this->load->view('base/footer');
+    }
 }
 
 ?>
---
+++ application/views/static/compensate.php
@@ -0,0 +1,29 @@
+<h2 style="text-align: center;">哈啰清华水果团购赔偿说明</h2><br/>
+<div style="font-size: 16px;">
+<span style="margin-left: 32px;"></span>哈啰清华（www.hellothu.com）致力于为清华的同学打造方便快捷、质量可靠、价格实惠的校园综合服务平台。哈啰水果团购自第四周上线以来，受到了广大在校师生的关注和支持。在服务同学，互惠双赢的过程中，同学们的宝贵建议和热情反馈，给了哈啰团队极大的帮助。
+<div style="margin-top: 9px;"></div>
+<span style="margin-left: 32px;"></span>我们尽自己最大的努力为同学们提供物美价廉的水果，但是不排除我们的一些人为失误造成一些质量问题。3月20日，一位用户在意见反馈及朋友圈中，指出了团队水果的质量问题。
+<br/>
+<img src="/page/img/compensate/step1.png" width="400" style="margin-left: 30px;"></h4>
+<div style="margin-top: 9px;"></div>
+
+<span style="margin-left: 32px;"></span>哈啰团队成员收到反馈后，立即与供货商联系，准备了用于赔偿的水果。几经周折，客服人员联系到该用户，很快解决了该问题。
+<br/>
+<img src="/page/img/compensate/step2.png" width="400" style="margin-left: 30px;"></h4>
+<div style="margin-top: 9px;"></div>
+
+<span style="margin-left: 32px;"></span>在水果团购中，哈啰清华团队始终将水果质量放在第一位，个别质量问题的出现，促使我们团队及时反思，积极进步，做出高效快捷的反应机制。在此，哈啰清华团队针对水果质量为题做出几点说明：
+<div style="margin-top: 9px;"></div>
+
+<span style="margin-left: 32px;"></span>1、希望同学们在拿到水果的时候当场进行检查，如果水果质量有问题，我们会立即更换水果；<br/>
+<span style="margin-left: 32px;"></span>2、如果水果本身无质量问题，在配送过程中因颠簸、挤压等发生变形等问题，通过各种方式与我们缺的联系后，我们将为您<b style="font-size:19px;">免费更换水果</b>；<br/>
+<span style="margin-left: 32px;"></span>3、如果水果本身有质量问题，与我们取得联系后，我们将退还有质量问题的水果费用，<b style="font-size:19px;">并赔偿等价于有问题水果三倍价钱的水果</b>；<br/>
+<div style="margin-top: 9px;"></div>
+
+<span style="margin-left: 32px;"></span>水果的口味可能会因为产地和季节而有所不同，味道甜美的水果也有可能外表看上去不是非常的漂亮，我们将尽我们最大的努力为同学们提供最优质的水果，我们也继续欢迎大家遇到问题随时向我们反馈，感谢您的理解！<br/>
+<div style="margin-top: 49px;"></div>
+
+<span style="margin-left: 32px;"></span>水果质量投诉电话：18010151107<br/>
+<span style="margin-left: 32px;"></span>网 站 客 服 邮 箱：hellothu@foxmail.com</br>
+<span style="margin-left: 32px;"></span>网站客服&洽谈电话：15311557983</br>
+</div>
