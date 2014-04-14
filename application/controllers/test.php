3a5753b4394e3195131f0f53f17defa04ed3edaa
ljnanest <ljnanest@gmail.com>
2014-04-14 16:04:54 +0800

    alipay

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -41,7 +41,7 @@ class Test extends CI_Controller{
 
     function test_alipay() {
         $this->load->model("alipay_model","alipay");
-        echo $this->alipay->do_alipay("测试1","描述1",0.10,123456789);
+        echo $this->alipay->do_alipay("测试1","描述1","0.10",123456710);
     }
 
 }
