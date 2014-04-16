03abc26b72bc794e3d2df7e915c6a82f075308db
ljnanest <ljnanest@gmail.com>
2014-04-16 17:25:57 +0800

    alipa

--- application/controllers/alipay.php
+++ application/controllers/alipay.php
@@ -130,11 +130,73 @@ class alipay extends CI_Controller {
     
     //确保公网能访问
     public function do_notify() {
-        include_once APPPATH.'third_party/alipay/alipay_submit.class.php';
-        return $alipaySubmit = new AlipaySubmit($this->alipay_config);
- 
-//        include_once APPPATH.'third_party/alipay/alipay_notify.class.php';  
-//        return APPPATH.'third_party/alipay/alipay_notify.class.php';  
+        include_once APPPATH.'third_party/alipay/alipay_notify.class.php';
+        $alipayNotify = new AlipayNotify($this->alipay_config);
+        //$verify_result = $alipayNotify->verifyReturn();
+        $verify_result = $this->check_alipay_request($_GET['notify_id']);
+
+        if($verify_result) {
+            //商户订单号
+            $out_trade_no = $_POST['out_trade_no'];
+            //支付宝交易号
+            $trade_no = $_POST['trade_no'];
+            //交易状态
+            $trade_status = $_POST['trade_status'];
+
+            if($_POST['trade_status'] == 'WAIT_BUYER_PAY') {//该判断表示买家已在支付宝交易管理中产生了交易记录，但没有付款
+                //判断该笔订单是否在商户网站中已经做过处理
+
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+
+                //如果有做过处理，不执行商户的业务程序
+
+                echo "success";     //请不要修改或删除
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            } else if($_POST['trade_status'] == 'WAIT_SELLER_SEND_GOODS') {
+                //该判断表示买家已在支付宝交易管理中产生了交易记录且付款成功，但卖家没有发货
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+                $this->setOrderAlipay($out_trade_no,"FINISHED");
+                echo "success";     //请不要修改或删除
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            } else if($_POST['trade_status'] == 'WAIT_BUYER_CONFIRM_GOODS') {
+                //该判断表示卖家已经发了货，但买家还没有做确认收货的操作
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+                echo "success";     //请不要修改或删除
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            } else if($_POST['trade_status'] == 'TRADE_FINISHED') {
+                //该判断表示买家已经确认收货，这笔交易完成
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+                
+                $this->setOrderAlipay($out_trade_no,"FINISHED");
+                echo "success";     //请不要修改或删除
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            } else {
+                //其他状态判断
+
+                echo "success";
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult ("这里写入想要调试的代码变量值，或其他运行的结果记录");
+                                                            
+            }
+               
+        } else {
+            //验证失败
+            echo "fail";
+
+            //调试用，写文本函数记录程序运行情况是否正常
+            //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+
+        }
     }
 
     private function check_alipay_request($notify_id_tmp){
@@ -144,6 +206,17 @@ class alipay extends CI_Controller {
                                               'notify_id'=>$notify_id_tmp
                                               )) == 'true'?true: false;
     }
+    
+    private function setOrderAlipay($out_trade_no,$str) {
+        $no_sign = substr($out_trade_no,0,2);
+        $orderID = substr($out_trade_no,2);
+        if ($no_sign == "gb") {
+            $this->load->model('groupbuy_model','gb');
+            $order = $this->gb->getOrderById($orderID);
+            if ($order[0]['alipay'] != "FINISHED") $this->gb->setOrderAlipayByID($orderID,$str);
+        }
+   }
+                                            
     
 }
 
