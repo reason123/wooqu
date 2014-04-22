67ee0e40f1227a5bb04f71e4d0e26a5902007187
ljnanest <ljnanest@gmail.com>
2014-04-22 17:26:53 +0800

    email

--- application/views/userpage/myBundling.php
+++ application/views/userpage/myBundling.php
@@ -2,9 +2,15 @@
 <div class="row">
     <div class="col-lg-3">
         <div class="input-group">
-            <input type="text" id = 'email' placeholder='填写您的邮箱' class="form-control"></input>
+            <input type="text" id = 'email' placeholder='填写您的邮箱' class="form-control" 
+            <?php
+                if ($userInfo['veri_email'] != NULL) 
+                    echo "value = '".$userInfo['veri_email']."' disabled='disabled'" ;
+                else if ($userInfo['email'] != NULL)
+                    echo "value = '".$userInfo['email']."'";
+            ?>></input>
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button" onclick="emailBundling()">绑定邮箱</button>
+                <button class="btn btn-default" type="button" <?php if ($userInfo['veri_email'] == NULL) echo "onclick='emailBundling()'" ?>><?php if ($userInfo['veri_email']!=NULL )echo "已"?>绑定邮箱</button>
             </span>
         </div>  
     </div>
